deb03f39bdfc0d1e3aeaff63e0a6837d35b90a90
Nick Phelps <nickphelps34@gmail.com>
2022-08-19 17:37:03 -0400

    commit tree 10bcef1d4639ac3d09ed31ccc4a9a9b63ba25430

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+    return a + b
+};
+function subtract(b, a){
+    return b - a
+};
+function multiply(a, b){
+    return a * b
+};
+function divide(a, b){
+    return a / b
+};
+function increment(n){
+    return (n ++ + 1)
+};
+function decrement(n){
+    return (n -- -1)
+};
+function makeInt(n){
+    console.log(parseInt(n, 10))
+    return (parseInt(n,10))
+};
+function preserveDecimal(n){
+    console.log(parseFloat(n))
+    return(parseFloat(n))
+};
